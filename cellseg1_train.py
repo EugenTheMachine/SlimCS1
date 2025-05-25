94490ec003fb0ca6e0b3d750be90ed509cacd55e
eugen <lazoskal24@gmail.com>
2025-05-25 15:44:35 +0300

    -

--- cellseg1_train.py
+++ cellseg1_train.py
@@ -10,8 +10,8 @@ import torch.optim as optim
 import yaml
 from torch.optim.lr_scheduler import OneCycleLR
 from torch.utils.data import DataLoader
-# from tqdm.notebook import tqdm
-from tqdm import tqdm
+from tqdm.notebook import tqdm
+# from tqdm import tqdm
 
 from cell_loss import cell_prob_mse_loss, cross_entropy_loss
 from data.dataset import TrainDataset
